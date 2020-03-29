16404b4a3a7f085cc3cae7a998c3b7247c32cba1
Valentin Berlier <berlier.v@gmail.com>
2020-03-29 04:58:02 +0200

    Don't test against other servers for now

--- tests/test_client.py
+++ tests/test_client.py
@@ -11,11 +11,6 @@ async def test_connect():
         pass
 
 
-async def test_connect_server():
-    async with Client.connect("smogtours"):
-        pass
-
-
 async def test_connect_host():
     async with Client.connect(host="sim.smogon.com:8000"):
         pass
