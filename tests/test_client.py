7e161dd4bcc3f907d0b2c72f06a99eeab7f3dd3f
Valentin Berlier <berlier.v@gmail.com>
2020-03-29 01:16:48 +0100

    Add more connection tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,3 +9,19 @@ pytestmark = pytest.mark.asyncio
 async def test_connect():
     async with Client.connect():
         pass
+
+
+async def test_connect_server():
+    async with Client.connect("smogtours"):
+        pass
+
+
+async def test_connect_host():
+    async with Client.connect(host="sim.smogon.com:8000"):
+        pass
+
+
+async def test_connect_uri():
+    uri = "ws://sim.smogon.com:8000/showdown/123/abcdefgh/websocket"
+    async with Client.connect(uri=uri):
+        pass
