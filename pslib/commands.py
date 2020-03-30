8d42dccf01744e007cbfb9e06aba5b9270ccd895
Valentin Berlier <berlier.v@gmail.com>
2020-03-30 02:52:16 +0200

    Handle query_battles parameters

--- pslib/commands.py
+++ pslib/commands.py
@@ -5,8 +5,13 @@ from .messages import QueryResponseMessage
 
 
 class GlobalCommandsMixin:
-    async def query_battles(self):
-        async with self.client.send_command("query", "roomlist"):
+    async def query_battles(self, format="", minimum_elo=None, username_prefix=""):
+        async with self.client.send_command(
+            "cmd roomlist",
+            format,
+            "none" if minimum_elo is None else minimum_elo,
+            username_prefix,
+        ):
             async for response in self.listen(QueryResponseMessage):
                 if response.querytype == "roomlist":
                     return [
