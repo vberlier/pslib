3a373f0e27ad9a800efb138d7d11e627bc5e8e4d
Valentin Berlier <berlier.v@gmail.com>
2020-03-30 23:08:38 +0200

    Try to test private messages in travis

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,34 +1,51 @@
 import pytest
+import asyncio
 
-from pslib import Client, ServerConnectionFailed
-
-
-pytestmark = pytest.mark.skip  # Temporarily skip client tests
+from pslib import connect, ServerConnectionFailed, PrivateMessage
 
 
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.mark.timeout(30)
 async def test_connect():
-    async with Client.connect():
-        pass
+    async def log(name, client):
+        async for message in client.listen(all_rooms=True):
+            print(name, message.serialize())
+
+    async def main(c1, c2):
+        await asyncio.gather(c1.login("pslib-dev-ci-1"), c2.login("pslib-dev-ci-2"))
+        print("SENDING")
+        await c1.private_message("pslib-dev-ci-2", "hello")
+        print("SUCCESS")
+        await asyncio.sleep(3)
+        assert False
+
+    async with connect() as c1, connect() as c2:
+        await asyncio.gather(log("c1", c1), log("c2", c2), main(c1, c2))
 
 
 async def test_connect_host():
-    async with Client.connect(host="sim.smogon.com:8000"):
+    async with connect(host="sim.smogon.com:8000"):
         pass
 
 
+@pytest.mark.skip
 async def test_connect_uri():
     uri = "ws://sim.smogon.com:8000/showdown/123/abcdefgh/websocket"
-    async with Client.connect(uri=uri):
+    async with connect(uri=uri):
         pass
 
 
+@pytest.mark.skip
 async def test_connect_nonsticky_uri():
     with pytest.raises(ServerConnectionFailed):
-        async with Client.connect(uri="ws://sim.smogon.com:8000/showdown/websocket"):
+        async with connect(uri="ws://sim.smogon.com:8000/showdown/websocket"):
             pass
 
 
+@pytest.mark.skip
 async def test_connect_nonsticky_uri_correct():
     uri = "ws://sim.smogon.com:8000/showdown/websocket"
-    async with Client.connect(uri=uri, sticky=False):
+    async with connect(uri=uri, sticky=False):
         pass
